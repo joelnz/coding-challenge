28c7042642610082be3a07869156d683e37bef64
Joel <joel@soot.com>
2023-08-08 20:37:38 +0200

    Added Earth orbit animation loop

--- src/babylon/setup-scene/index.ts
+++ src/babylon/setup-scene/index.ts
@@ -1,4 +1,6 @@
 import {
+  AnimationGroup,
+  Animation,
   Color3,
   Color4,
   FreeCamera,
@@ -11,14 +13,14 @@ import {
 import '@babylonjs/loaders/glTF';
 
 export const onSceneReady = (scene: Scene) => {
-  scene.clearColor = Color4.FromHexString("#010b19");
+  scene.clearColor = Color4.FromHexString('#010b19');
   const camera = new FreeCamera('camera', new Vector3(0, 0, -8), scene);
   camera.setTarget(Vector3.Zero());
 
   const canvas = scene.getEngine().getRenderingCanvas();
   camera.attachControl(canvas, true);
 
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
   light.groundColor = Color3.White();
   light.intensity = 0.7;
 
@@ -40,16 +42,44 @@ export const onSceneReady = (scene: Scene) => {
     },
   );
 
+  // Load Earth model
   SceneLoader.ImportMesh(
     '',
     'models/',
     'Earth.glb',
     scene,
     (meshes) => {
+      // Create an animation group
+      const animationGroup = new AnimationGroup('EarthOrbit');
       meshes.forEach((mesh) => {
         mesh.position = new Vector3(4, 0, 0);
         mesh.scaling = new Vector3(0.02, 0.02, 0.02);
+        // Create Earth's orbit animation
+        const orbitAnimation = new Animation(
+          'orbit',
+          'position',
+          60,
+          Animation.ANIMATIONTYPE_VECTOR3,
+          Animation.ANIMATIONLOOPMODE_CYCLE,
+        );
+
+        // Animation keys
+        const keys = [];
+        for (let frame = 0; frame <= 360; frame += 10) {
+          const angle = frame * (Math.PI / 180);
+          const x = 4 * Math.cos(angle);
+          const z = 4 * Math.sin(angle);
+          keys.push({ frame, value: new Vector3(x, 0, z) });
+        }
+        orbitAnimation.setKeys(keys);
+
+        // Add orbit animation to the mesh and the animation group
+        mesh.animations.push(orbitAnimation);
+        animationGroup.addTargetedAnimation(orbitAnimation, mesh);
       });
+      
+      // Play animation group in a loop
+      animationGroup.play(true);
     },
     null,
     (error) => {
