d36959141329f12e66ad2b356974a55b1faafcfd
Joel <joel@soot.com>
2023-08-09 15:06:50 +0200

    Add PointLight at the position of sun

--- src/babylon/setup-scene/index.ts
+++ src/babylon/setup-scene/index.ts
@@ -1,40 +1,38 @@
 import {
-  AnimationGroup,
-  Animation,
-  Color3,
-  Color4,
-  CubeTexture,
   ArcRotateCamera,
-  HemisphericLight,
-  MeshBuilder,
+  Color3,
+  PointLight,
   Scene,
-  StandardMaterial,
-  Texture,
   Vector3,
-  SceneLoader,
 } from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 
-import Earth from './scene-components/Earth'
-import Skybox from './scene-components/Skybox'
-import Sun from './scene-components/Sun'
-
+import Earth from './scene-components/Earth';
+import Skybox from './scene-components/Skybox';
+import Sun from './scene-components/Sun';
 
 export const onSceneReady = (scene: Scene) => {
-  scene.clearColor = Color4.FromHexString('#ffffff');
-  const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2, 7, new Vector3(0, 0, 0), scene);
+  // Set up canvas
+  const canvas = scene.getEngine().getRenderingCanvas();
+  // Set up camera
+  const camera = new ArcRotateCamera(
+    'camera',
+    -Math.PI / 2,
+    Math.PI / 2,
+    7,
+    new Vector3(0, 0, 0),
+    scene,
+  );
+  camera.attachControl(canvas, true);
   camera.setTarget(Vector3.Zero());
 
-  const canvas = scene.getEngine().getRenderingCanvas();
-  camera.attachControl(canvas, true);
+  // Create PointLight at the position of sun
+  const light = new PointLight('light', new Vector3(0, 0, 0), scene);
+  light.diffuse = new Color3(1, 0.85, 0.6);
+  light.intensity = 50;
 
-  const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
-  light.groundColor = Color3.White();
-  light.intensity = 0.7;
-
+  // Add scene components
   Skybox(scene);
   Sun(scene);
   Earth(scene);
-
-  
 };
